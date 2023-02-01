14488a6c323764b46eaa61a5adb958443e369b0d
Artem Danytskyi <artemdanitskiy@gmail.com>
2023-02-01 10:41:35 +0200

    change color

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-const storageValue = document.querySelector("#storage_value");
-const storageInput = document.querySelector("#storage");
-const transferValue = document.querySelector("#transfer_value");
-const transferInput = document.querySelector("#transfer");
-const backblaze_chart = document.querySelector("#backblaze_chart");
-const bunny_chart = document.querySelector("#bunny_chart");
-const scaleway_chart = document.querySelector("#scaleway_chart");
-const vultr_chart = document.querySelector("#vultr_chart");
-const backblaze_value = document.querySelector("#backblaze_value");
-const bunny_value = document.querySelector("#bunny_value");
-const scaleway_value = document.querySelector("#scaleway_value");
-const vultr_value = document.querySelector("#vultr_value");
-const backblazeStorageCost = 0.005;
-const backblazeTransferCost = 0.01;
-const backblazeMin = 7;
-const bunnyStorageHDDCost = 0.01;
-const bunnyStorageSSDCost = 0.02;
-const bunnyTransfer = 0.01;
-const bunnyMax = 10;
-const scalewayStorageMulti = 0.06;
-const scalewayStorageSingle = 0.03;
-const scalewayTransfer = 0.02;
-const vultrStorage = 0.01;
-const vultrTransfer = 0.01;
-const scalewayMultiFree = 75;
-const scalewaySingleFree = 75;
+// storageValue.textContent = storageInput.value;
+// transferValue.textContent = transferInput.value;
 
-let STOR;
-let TRANS;
+// //default values:
+// let backblazeStorage = +storageInput.value * backblazeStorageCost;
+// let backblazeTransfer = +transferInput.value * backblazeTransferCost;
+// let backblazeTotal =
+//     Math.round((backblazeStorage + backblazeTransfer) * 100) / 100;
+// backblaze_value.textContent = backblazeTotal;
+// backblaze_chart.style.width = `${backblazeTotal}px`;
 
-storageInput.addEventListener("input", (event) => {
-    storageValue.textContent = event.target.value;
-    STOR = +storageValue.value;
-});
+// let bunnyStorage =
+//     +storageInput.value * (bunnyStorageHDDCost || bunnyStorageSSDCost);
+// let bunnyTransfer = +transferInput.value * bunnyTransferCost;
+// let bunnyTotal = (bunnyStorage + bunnyTransfer).toFixed(2);
+// bunny_value.textContent = bunnyTotal;
+// bunny_chart.style.width = `${bunnyTotal}px`;
 
-transferInput.addEventListener("input", (event) => {
-    transferValue.textContent = event.target.value;
-    TRANS = +transferValue.value;
-});
+// let scalewayStorage =
+//     +storageInput.value *
+//     (scalewayStorageMultiCost || scalewayStorageSingleCost);
+// let scalewayTransfer = +transferInput.value * scalewayTransferCost;
+// let scalewayTotal =
+//     Math.round((scalewayStorage + scalewayTransfer) * 100) / 100;
+// console.log(scalewayTotal);
+// scaleway_value.textContent = scalewayTotal;
+// scaleway_chart.style.width = `${scalewayTotal}px`;
+
+// let vultrStorage = +storageInput.value * vultrStorageCost;
+// let vultrTransfer = +transferInput.value * vultrTransferCost;
+// let vultrTotal = (backblazeStorage + backblazeTransfer).toFixed(2);
+// vultr_value.textContent = vultrTotal;
+// vultr_chart.style.width = `${backblazeTotal}px`;
+//CALCULATE TOTAL COST:
+
+//backblaze calc:
+
+//bunny calc:
+
+// bunny switch (hdd/ssd):
